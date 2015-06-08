fe69aa2edec02a138a75e07828e291055419cd27
Jakub Dzon <jakub@dzon.pl>
2015-06-08 23:20:56 +0200

    Model development

---
+++ src/main/java/org/singularitylab/infakt/client/model/Currency.java
@@ -0,0 +1,11 @@
+package org.singularitylab.infakt.client.model;
+
+/**
+ * @author Jakub Dzon
+ */
+public enum Currency {
+    THB, USD, AUD, HKD, CAD, NZD, SGD, EUR, HUF,
+    CHF, GBP, UAH, JPY, CZK, DKK, ISK, NOK, SEK,
+    HRK, RON, BGN, TRY, LTL, LVL, PHP, MXN, ZAR,
+    BRL, MYR, RUB, IDR, KRW, CNY, INR, PLN
+}
--- src/main/java/org/singularitylab/infakt/client/model/Invoice.java
+++ src/main/java/org/singularitylab/infakt/client/model/Invoice.java
@@ -1,7 +1,21 @@
 package org.singularitylab.infakt.client.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * @author Jakub Dzon
  */
 public class Invoice {
+    private int id;
+
+    private String number;
+
+    private Currency currency;
+
+    @JsonProperty("paid_price")
+    private int paidPrice;
+
+    private String notes;
+
+
 }
